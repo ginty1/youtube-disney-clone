eb5053e555923b8392e42fad0c0c225fba4d529f
ginty1 <chiombagintykondwani@gmail.com>
2021-05-09 20:29:16 +0200

    slinder settings

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -2,12 +2,67 @@ import styled from 'styled-components';
 import React from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import slider from "react-slick";
+import Slider from "react-slick";
 
 export default function ImgSlider(props) {
+
+  let settings={
+    dots:true,
+    infinite:true,
+    speed:500,
+    slidesToShow:1,
+    slidesToScrow:1,
+    autoplay:true,
+  }
     return (
-        <div>
-          image slider  
-        </div>
+         <Carousel {...settings}>
+            <div>
+              <h3>
+                1
+              </h3>
+            </div>
+            <div>
+              <h3>
+                2
+              </h3>
+            </div>
+            <div>
+              <h3>
+                3
+              </h3>
+            </div>
+            <div>
+              <h3>
+                5
+              </h3>
+            </div>
+
+          </Carousel>
+        
     )
-}
+};
+const Carousel=styled(Slider)`
+ margin-top:20px;
+ &> button{
+   opacity:0;
+   height:100%
+   width:5vw;
+   z-index:1;
+   &:hover{
+     opacity:1;
+     transition: opacity 0.2s ease 0s;
+   }
+  }
+
+
+  
+  ul li button{
+    &:before{
+      font-size: 40px;
+      color: rgb(150,158,171);
+      }
+    }
+  li.slick-active button:before {
+    color:white;
+  }
+`;
