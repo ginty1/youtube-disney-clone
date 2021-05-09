3d7b8ce32653f17cb7742ea9cc13459cf1d56911
ginty1 <chiombagintykondwani@gmail.com>
2021-05-09 18:55:39 +0200

    imports/css,theme from slick

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import slider from "react-slick";
 
 export default function ImgSlider(props) {
     return (
