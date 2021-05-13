4480319355d68300cea406bf532c93efc6e94742
ginty1 <chiombagintykondwani@gmail.com>
2021-05-13 05:43:22 +0200

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import styled from "styled-components"
 import ImgSlider from "./ImgSlider"
+import NewDisney from "./NewDisney"
+import Originals from "./Originals"
+import Recommends from "./Recommends"
+import Trending from "./Trending"
+import Viewers from "./Viewers"
 
 function Home(props) {
     return (
         <Container>
-            <ImgSlider/>         
+            <ImgSlider/> 
+            <Viewers/>   
+            <Recommends/> 
+            <NewDisney/>   
+            <Originals/>
+            <Trending/> 
         </Container>
     )
 }
