8280248b3e346625434fd1fe68a9692322b77f58
ginty1 <chiombagintykondwani@gmail.com>
2021-05-21 21:51:06 +0200

    data retrival from firebase/ Movies Coll

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,64 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import styled from "styled-components"
-import ImgSlider from "./ImgSlider"
-import NewDisney from "./NewDisney"
-import Originals from "./Originals"
-import Recommends from "./Recommends"
-import Trending from "./Trending"
-import Viewers from "./Viewers"
+import { selectUserName } from "../features/users/userSlice"
+import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Recommends from "./Recommends";
+import Trending from "./Trending";
+import Viewers from "./Viewers";
+import db from "../firebase/firebase";
+import { setMovies } from "../features/movie/movieSlice";
 
 function Home(props) {
+
+    const dispatch=useDispatch();
+    const userName=useSelector(selectUserName);
+    let recommends=[];
+    let newDisneys=[];
+    let originals=[];
+    let trending=[];
+
+    useEffect(()=>{
+        //puling data from firebase collection
+        db.collection('Movies').onSnapshot((snapshot)=>{
+            //mapping through the doc
+         snapshot.docs.map((doc)=>{
+           // console.log(recommends);
+                    switch(doc.data().type){
+                        //when type in db is recommened
+                   case 'recommends':
+                   recommends=[...recommends, {id: doc.id, ...doc.data()}]; 
+                   
+                       break;
+                   case 'newDisneys':
+                         //when type in db is newDisneys
+                    newDisneys=[...newDisneys, {id: doc.id, ...doc.data()}] ;
+                    
+                       break;
+                   case 'originals':
+                       //when type in db originals
+                    originals=[...originals, {id: doc.id, ...doc.data()}] ;
+                     break;
+                   case 'trending':
+                       //when type in db trending
+                    trending=[...trending, {id: doc.id, ...doc.data()}] ;
+                       break;
+               }
+           }); 
+           dispatch(
+               setMovies({
+                   recommend:recommends,
+                   newDisney:newDisneys,
+                   original:originals,
+                   trending:trending,
+                })
+           );
+        });
+    },[userName]);
+
+
     return (
         <Container>
             <ImgSlider/> 
