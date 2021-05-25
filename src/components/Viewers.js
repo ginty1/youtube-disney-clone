8231d480c8faa9bd0f3ea49326e204f6d5409974
ginty1 <chiombagintykondwani@gmail.com>
2021-05-25 16:28:25 +0200

    edited styles

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -26,7 +26,7 @@ function Viewers(props) {
             </Wrap>
             <Wrap>
                 <img src="/img/viewers-starwars.png" alt=""/>
-                <video autoPlay={true} loop={true} playsInline={true}>
+                 <video autoPlay={true} loop={true} playsInline={true}>
                     <source src="/videos/1608229455-star-wars.mp4" type="video/mp4"/>
                 </video>
                 
@@ -35,21 +35,20 @@ function Viewers(props) {
                 <img src="/img/viewers-national.png" alt=""/>
                 <video autoPlay={true} loop={true} playsInline={true}>
                     <source src="/videos/1564676296-national-geographic.mp4" type="video/mp4"/>
-                </video>
+                </video> 
             </Wrap>
         </Container>
     )
 };
 const Container=styled.div`
-margin-top: 30px;
+margin-top: 10px;
 padding: 30px 0px 26px;
 display: grid;
 grid-gap:25px;
-gap: 25px;
 grid-template-columns: repeat(5, minmax(0, 1fr)); 
 
 @media (max-width: 768px){
-    grid-template-columns: repeat(1,minmax(0, 1fr));
+    grid-template-columns: repeat(1, minmax(0, 1fr));
 }
 `
 const Wrap=styled.div`
@@ -63,7 +62,7 @@ position: relative;
 transition:all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
 border:3px solid rgba(249,249,249,0.1);
 img{
-    inset: 0px;
+    inset:0px;
     display:block;
     height:100%;
     object-fit:cover;
@@ -74,22 +73,25 @@ img{
     z-index:1;
     top:0;
 }
-video {  
- width:100%
- height:100%
- position:absolute;
- top:0px;  
- opacity:0;
- z-index:0; 
+
+video{
+    width:100%;
+    height:100%;
+    position:absolute;
+    top: 0px;
+    opacity:0;
+    z-index:0;
+
 }
-&:hover {
+&:hover{
     box-shadow:rgb(0 0 0/80%) 0px 40px 58px -16px,
-      rgb(0 0 0/72%) 0px 30px 22px -10px;
-      transform: scale(1.05);
-      border-color:rgba(249,249,249,0.8);
-      video{
-          opacity:1;
-      }
+     rgb(0 0 0 /72%) 0px 30px 22px -10px;
+     transform: scale(1.05);
+     border-color:rgba(249,249,249,0.8);
+    video{
+        opacity:1;
+    }
 }
-`
+
+`;
 export default Viewers
